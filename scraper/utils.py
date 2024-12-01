827d33fe06066d4713ac675546c4c4aeddb8b43b
William Gregorio <mail@williamgregorio.com>
2024-12-01 10:19:40 -0500

    added doc, and new helper function on on robots else stop

--- scraper/utils.py
+++ scraper/utils.py
@@ -3,6 +3,14 @@ from fake_useragent import UserAgent
 from urllib.parse import urljoin
 
 def get_headers():
+    """
+    Generates a 'random' user agent header....
+    """
     ua = UserAgent()
     return {'User-Agent': ua.random}
 
+def check_robots(domain):
+    """
+    Checks disallowed on robots.txt when crawling.
+    """
+    robots_url = urljoin(domain, '/robots.txt')
