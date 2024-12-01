71f90c61f5dac760b8835411f10ed05ac3dfdc6a
William Gregorio <mail@williamgregorio.com>
2024-12-01 10:48:18 -0500

    configin the output

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,4 +6,17 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from scraper.utils import get_headers, check_robots
 
+class EmailScraper:
+    def __init__(self, domain):
+        self.domain = domain.rstrip('/')
+        self.visited_urls = set()
+        self.emails = []
+        self.output_dir = self.prepare_output_directory()
 
+    def prepare_output_directory(self):
+        """
+        We will prepare the output directory of data for the domain.
+        """
+        base_dir = 'emails'
+        domain_dir = os.path.join(base_dir, urlparse(self.domain).netloc)
+        os.makedirs(domain_dir, exists_ok=True)
