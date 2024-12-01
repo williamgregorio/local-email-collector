44906cb61a93c18298aac98c660e53cd3ee9e3c9
William Gregorio <mail@williamgregorio.com>
2024-12-01 10:24:54 -0500

    added a simp checker on robots return bool

--- scraper/utils.py
+++ scraper/utils.py
@@ -14,3 +14,12 @@ def check_robots(domain):
     Checks disallowed on robots.txt when crawling.
     """
     robots_url = urljoin(domain, '/robots.txt')
+    try:
+        response = requests.get(robots_url, timeout=7)
+        if response.status_code == 200:
+            if 'Disallow: /' in response.text:
+                print(f"Crawling is disallowed for {domain} by robots.txt")
+                return False
+        return True
+    except Exception as e:
+        print(f"Failed to get robots.txt for {domain}: {e}")
